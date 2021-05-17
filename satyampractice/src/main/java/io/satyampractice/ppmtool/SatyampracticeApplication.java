704eb1ac57d8710399c091e253b401bbf95ddba5
Satyam <satyam.nagpure@icloud.com>
2021-05-17 17:08:53 +0530

    second commit

---
+++ satyampractice/src/main/java/io/satyampractice/ppmtool/SatyampracticeApplication.java
@@ -0,0 +1,13 @@
+package io.satyampractice.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatyampracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatyampracticeApplication.class, args);
+	}
+
+}
